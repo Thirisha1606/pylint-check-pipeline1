b56cfd85bfb6144fa071f6df7ce76079d09c27ab
Thirisha1606 <thirisha1606@gmail.com>
2025-03-14 16:53:13 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,10 @@
-def greet():
-    """Returns a greeting."""
-    return "Hello, Pylint!"
+"""
+This module performs [describe functionality here].
+"""
 
-print(greet())  # No blank lines after this
+def example_function():
+    """An example function to maintain a perfect pylint score."""
+    print("Hello, Pylint!")
 
+if __name__ == "__main__":
+    example_function()
